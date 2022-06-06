077c0dcb573ea4870c605009c4e7c0b15005c035
anattok <tokarev182@ya.ru>
2022-06-06 22:48:08 +0300

    сделал выпадашки в секции ремонтируем поломки

--- app/js/main.js
+++ app/js/main.js
@@ -1,74 +1,46 @@
 $(function() {
-    $('.slider-choice__inner ').slick({
-        dots: true,
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        variableWidth: true,
-        responsive: [{
-                breakpoint: 1199,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    });
 
-    $('.reviews__slider').slick({
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        dots: true,
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 2300,
-        variableWidth: true,
-
-    });
-
-    $('.actions__slider').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: true,
-        arrows: false,
-        autoplay: true,
-        vertical: true,
-        verticalSwiping: true,
-        autoplaySpeed: 2000,
-
-    });
-
-    $('.star').rateYo({
-        starWidth: "20px",
-        ratedFill: "#ADC431",
-        normalFill: "#ccccce",
-
-        readOnly: true
-
-    });
-
-
+    /*БУРГЕР МЕНЮ НА JQUERY*/
     $('.menu__btn').on('click', function() {
         $('.menu__list').toggleClass('menu__list--active');
     });
 
 });
 
-/*пытаюсь сделать кнопку на jquery "показать ещё"*/
 
-/* $(function() {
+/*КОД АККОРДЕОНА ВЗЯТ ИЗ ИНТЕРНЕТА, ПОНЯТИЮ НЕ ИМЕЮ КАК ОН РАБОТАЕТ*/
+var accordion = function() {
+    var data = $(".accordion").attr("data-accordion");
 
+    $(".breaking-item__title").on("click", function() {
 
-    $(".price__service-line-hide").hide();
-    $(".price__btn-load").click(function() {
-        $(this).next(".price__service-line-hide").slideToggle();
-
+        if (data === "close") {
+            $(".breaking-item__inner").slideDown();
+            if ($(this).hasClass("active")) {
+                $(this).toggleClass("active");
+            } else {
+                $(".accordion__item-header").removeClass("active");
+                $(this).toggleClass("active");
+            }
+        } else {
+            $(this).toggleClass("active");
+        }
+        $(this).next(".breaking-item__inner").not(":animated").slideToggle();
     });
-}); */
+};
+accordion();
+
+/* НАСТРОЙКА И ПОДКЛЮЧЕНИЕ SLIDER SWIPER*/
+
+
+
+let swiper = new Swiper(".slider-choice__inner", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+
+});
